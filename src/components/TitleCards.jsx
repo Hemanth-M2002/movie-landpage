b1ac6a330f3f0691603811bac8aa10dfe484e89e
Hemanth-M2002 <heamnthmathivanan02@gmail.com>
2024-06-01 15:03:35 +0530

    first commit

--- src/components/TitleCards.jsx
+++ src/components/TitleCards.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 
@@ -11,7 +9,7 @@ const TitleCards = ({ title, category }) => {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmE4MGY2MWU0NmYxYzU0YTYwYTM5ODM4NTk4OWJjMiIsInN1YiI6IjY1YTA1MGY2MWU0NmYxYzU0YTYwYTM5ODM4NTk4OWJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dwJzEHJaJqEsKdqY_2OJHevY-VyvSEGYoBt0Q3NaSfM'
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmE4MGY2MWU0NmYxYzU0YTYwYTM5ODM4NTk4OWJjMiIsInN1YiI6IjY2NWExNDNhZGM4ZWZlZTQ2NzFkNThiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dwJzEHJaJqEsKdqY_2OJHevY-VyvSEGYoBt0Q3NaSfM'
         }
     };
 
@@ -32,35 +30,36 @@ const TitleCards = ({ title, category }) => {
     };
 
     useEffect(() => {
-      fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
-          .then(response => response.json())
-          .then(response => {
-              setApiData(response.results);
-              console.log(response.results); // Log the data here
-          })
-          .catch(err => console.error(err));
-  }, [category]);
-  
+        fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+            .then(response => response.json())
+            .then(response => {
+                if (response.results) {
+                    setApiData(response.results);
+                } else {
+                    console.error('Unexpected API response structure:', response);
+                }
+            })
+            .catch(err => console.error('API fetch error:', err));
+    }, [category]);
+
     return (
         <div className='titlecards'>
             <h2>{title ? title : "Popular on Netflix"}</h2>
             <div className="card-container">
                 <button className="scroll-button left" onClick={scrollLeft}>{"<"}</button>
                 <div className="card-list" ref={cardsRef}>
-                    {apiData.map((card, index) => {
-                        return (
-                            <div className="card" key={index}>
-                                <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} className="card-img" alt={card.original_title} />
-                                <div className="card-overlay">
-                                    <p>{card.original_title}</p>
-                                    <div className="card-buttons">
-                                        <button className="play-button">Play</button>
-                                        <button className="add-button">Add to List</button>
-                                    </div>
+                    {apiData.map((card, index) => (
+                        <div className="card" key={index}>
+                            <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} className="card-img" alt={card.original_title} />
+                            <div className="card-overlay">
+                                <p>{card.original_title}</p>
+                                <div className="card-buttons">
+                                    <button className="play-button">Play</button>
+                                    <button className="add-button">Add to List</button>
                                 </div>
                             </div>
-                        );
-                    })}
+                        </div>
+                    ))}
                 </div>
                 <button className="scroll-button right" onClick={scrollRight}>{">"}</button>
             </div>
